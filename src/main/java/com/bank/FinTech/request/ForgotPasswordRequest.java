17fda041de74d3bf37043a162c7c91b18f715f9f
tekfors <offorsco@gmail.com>
2023-02-16 16:23:26 +0100

    setup

---
+++ src/main/java/com/bank/FinTech/request/ForgotPasswordRequest.java
@@ -0,0 +1,17 @@
+package com.bank.FinTech.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ForgotPasswordRequest {
+    @Email
+    private String email;
+}
